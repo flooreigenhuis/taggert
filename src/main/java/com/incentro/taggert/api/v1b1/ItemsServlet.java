eac6c204d5ef7cebf11685a27355aae4ef6ff60a
Floor <flooreigenhuis@Floors-MacBook-Pro.local>
2019-01-14 16:49:14 +0100

    ItemsServlet now serves random items on refresh page

--- src/main/java/com/incentro/taggert/api/v1b1/ItemsServlet.java
+++ src/main/java/com/incentro/taggert/api/v1b1/ItemsServlet.java
@@ -9,6 +9,10 @@ package com.incentro.taggert.api.v1b1;
         import javax.servlet.http.HttpServletRequest;
         import javax.servlet.http.HttpServletResponse;
         import java.io.IOException;
+        import java.util.Arrays;
+        import java.util.List;
+
+        import java.util.Random;
 
 
 @Singleton
@@ -16,31 +20,52 @@ public class ItemsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<String> values = returnValues();
+
+        LabelledItem labelitem = makeItem(values.get(0), values.get(1), values.get(2), values.get(3), values.get(4), values.get(5));
+
+        resp.getWriter().print(labelitem.toString());
+        resp.setContentType("application/json");
+    }
+
+    List<String> returnValues () {
+
+        List<String> id = Arrays.asList("default", "other", "bla1", "bla2");
+        List<String> url = Arrays.asList("http://google.nl", "http://wikipedia.com", "http://jsiojaio.nl", "http:ijsiojois.nl");
+        List<String> by = Arrays.asList("Floor", "Hayo", "Ted", "Doris");
+        List<String> label = Arrays.asList("shark", "rainbow shark", "cat", "dog");
+        List<String> name = Arrays.asList("img1", "img2", "img3", "img4");
+        List<String> parent = Arrays.asList("default", "dataset1", "dataset2", "dataset3");
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(id.size());
+
+        return Arrays.asList(id.get(randomNumber), url.get(randomNumber), by.get(randomNumber), label.get(randomNumber), name.get(randomNumber), parent.get(randomNumber));
+    }
+
+    protected LabelledItem makeItem(String id, String url, String by, String thislabel, String name, String parent) {
 
         Item.ImageLink link = Item.ImageLink.newBuilder()
-                .setId("default")
-                .setUrl("https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547")
+                .setId(id)
+                .setUrl(url)
                 .build();
 
-        Item thisitem = com.incentro.taggert.v1b1.Item.newBuilder()
-                .setName("Shark")
+        Item item = com.incentro.taggert.v1b1.Item.newBuilder()
+                .setName(name)
                 .setImageLink(link)
                 .build();
 
         LabelledItem.Labelling label = LabelledItem.Labelling.newBuilder()
-                .addLabels("rainbow shark")
-                .setBy("Floor")
+                .addLabels(thislabel)
+                .setBy(by)
                 .build();
 
-        LabelledItem labelitem = LabelledItem.newBuilder()
-                .setName("Shark")
-                .setParent("default")
-                .setItem(thisitem)
+
+        return LabelledItem.newBuilder()
+                .setName(name)
+                .setParent(parent)
+                .setItem(item)
                 .addLabellings(label)
                 .build();
-
-
-        resp.getWriter().print(labelitem.toString());
-        resp.setContentType("application/json");
     }
 }
